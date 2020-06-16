a9ac1899380e23178741c4dfd74ceed83ed419c1
Jonas Jäger <j.jaeger@stud.tu-darmstadt.de>
2020-06-16 23:59:06 +0200

    Spearman's rank correlation coefficient added as metric for model fitting.

--- run.py
+++ run.py
@@ -2,11 +2,15 @@ from model import Similarity
 from train import train_model
 from preprocessing import get_datasets
 from adversarial_attacks import VIPER_ICES, VIPER_DCES, VIPER_ECES, DisemvowelingAttack, AttackPipeline
-
-
+from scipy.stats import spearmanr
+import tensorflow as tf
 import numpy as np
 
 np.random.seed(100)
+
+
+def spearman_metric(y_true, y_pred):
+    return tf.py_function(spearmanr, [tf.cast(y_pred, tf.float32), tf.cast(y_true, tf.float32)], Tout=tf.float32)
 
 
 if __name__ == '__main__':
@@ -22,18 +26,21 @@ if __name__ == '__main__':
     if run_eagerly:
         train_model(model, train[0], np.array(train[0]))
     else:
-        model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+        model.compile(optimizer='adam', loss='mse', metrics=['mae', spearman_metric])
         history = model.fit(*train, validation_data=dev, epochs=400, batch_size=100)
 
         import matplotlib.pyplot as plt
-        training_loss = history.history['loss']
-        dev_loss = history.history['val_loss']
-        plt.plot(training_loss, 'r--')
-        plt.plot(dev_loss, 'b-')
-        plt.legend(['Training Loss', 'Dev Loss'])
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.show()
+        # Plot train & dev evaluation curve on different metrics:
+        for metric in ['loss', 'spearman_metric']:
+            training_loss = history.history[metric]
+            dev_loss = history.history[f"val_{metric}"]
+            plt.plot(training_loss, 'r--')
+            plt.plot(dev_loss, 'b-')
+            plt.legend(['Training set', 'Dev set'])
+            plt.xlabel('Epoch')
+            plt.ylabel(metric)
+            plt.title(metric)
+            plt.show()
 
     print(model.evaluate(*train, batch_size=100))
     print(model.evaluate(*dev, batch_size=100))
